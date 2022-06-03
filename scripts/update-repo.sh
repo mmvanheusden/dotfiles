2fafa2d83991cf8860ec70362c047e395b764e77
Maarten van Heusden <mmvanheusden@gmail.com>
2022-06-03 20:21:54 +0200

    add a warning to the script

--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
+echo "Please run this script from the dotfiles dir or it will cause errors!"
 parent_PWD=$(readlink -ve "/proc/$PPID/cwd")
 
-cp ~/.config/starship.toml $parent_PWD/../config/starship.toml
+cp ~/.config/starship.toml $parent_PWD/config/starship.toml
 echo "Moved starship config"
 
-cp ~/.config/fish/config.fish $parent_PWD/../config/config.fish
+cp ~/.config/fish/config.fish $parent_PWD/config/config.fish
 echo "Moved fish config"
